23b656de564d32354695ccd016af656ce0864cee
OlgaDobrydina <125290861+OlgaDobrydina@users.noreply.github.com>
2023-04-17 21:48:26 +0500

    Add files via upload

---
+++ HW_10.cs
@@ -0,0 +1,8 @@
+﻿//  Напишите программу, которая принимает на вход
+// трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.WriteLine("Введите трехзначное число ");
+string input = Console.ReadLine();
+int Number = int.Parse(input);
+int Digit1 = Number/10;
+int Digit2 = Digit1%10;
+Console.WriteLine(Digit2);
---
+++ HW_13.cs
@@ -0,0 +1,27 @@
+﻿//  Напишите программу, которая выводит третью цифру заданного числа
+// или сообщает, что третьей цифры нет.
+Console.WriteLine("Введите число ");
+string input = Console.ReadLine();
+int Number = int.Parse(input);
+int[] array = new int[input.Length];
+
+if (array.Length > 2)
+    {
+     int z = 0;
+        for (int i = array.Length - 1; i >= 0; i--)
+     {
+        z = Number % 10;
+        array[i] = z;
+        Number -= z;
+        Number = Number / 10;
+     }
+     for (int i = 0; i < array.Length; i++)
+     {
+     Console.WriteLine("Третья цифра " + array[2]);
+     break;
+     }   
+    }
+else
+{
+    Console.WriteLine("Нет третьей цифры ");
+}    
