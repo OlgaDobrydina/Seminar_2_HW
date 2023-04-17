df145a2848b33c8707759c7f989ba57d5ea15cc9
Olga Dobrydina <doa1981@mail.ru>
2023-04-17 21:45:23 +0500

    DZ 15

---
+++ HW_15.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели,
+// и проверяет, является ли этот день выходным.
+Console.WriteLine("Ведите цифру ");
+string input = Console.ReadLine();
+int Number = int.Parse(input);
+
+if (Number == 7) Console.WriteLine("Выходной");
+if (Number == 6) Console.WriteLine("Выходной");
+else
+{
+    if (Number > 7) Console.WriteLine("Нет такого дня недели");
+    else
+     {
+        if (Number == 1) Console.WriteLine("Это рабочий день");
+        if (Number == 2) Console.WriteLine("Это рабочий день");
+        if (Number == 3) Console.WriteLine("Это рабочий день");
+        if (Number == 4) Console.WriteLine("Это рабочий день");
+        if (Number == 5) Console.WriteLine("Это рабочий день");
+     }       
+}
